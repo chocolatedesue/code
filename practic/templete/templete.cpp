8b1ba9b7ea95db4953994856b4f59f6e7bf3b0be
ccds <1356708169@qq.com>
2022-06-25 07:47:37 +0800

    add prac

---
+++ practic/templete/templete.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int>PII;
+
+int n,m,k,T;
+
+
+
+void solve(){
+
+
+}
+
+int main(){
+
+    // freopen("input.txt","r");
+    // freopen("output.txt","w");
+    solve();
+    return 0;
+}
