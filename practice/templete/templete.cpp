bd4a44502698ead9e15dfb0ce6f801dad3b23df2
ccds <1356708169@qq.com>
2022-06-27 17:01:13 +0800

    "sad"

--- practice/templete/templete.cpp
+++ practice/templete/templete.cpp
@@ -4,6 +4,7 @@ using namespace std;
 typedef long long ll;
 typedef pair<int,int>PII;
 #define endl '\n'
+#define io ios::sync_with_stdio(false),cin.tie(0)
 const int INF = 0x3f3f3f3f;
 
 int n,m,k,T;
@@ -16,9 +17,11 @@ void solve(){
 }
 
 int main(){
-
+    
     // freopen("input.txt","r");
     // freopen("output.txt","w");
+    io;
+ 
     solve();
     return 0;
 }
